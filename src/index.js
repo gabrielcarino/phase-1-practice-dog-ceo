4d47e4080b3c4dbe4148e00cb6893b68afb632b8
gabriel Carino <gabejcarino@gmail.com>
2022-04-18 14:42:20 -0500

    commit tree 553a62be700fd37a8aaa80b9af8bd40152d6c8a0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  let breeds = []
+
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      data.message.forEach(dogImage => renderImage(dogImage))
+  })
+
+  fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      breeds = Object.keys(data.message)
+      renderBreeds(breeds)
+  })
+
+  const dropdown = document.querySelector('#breed-dropdown')
+  const ul = document.querySelector('#dog-breeds')
+
+  dropdown.addEventListener('change', handleChange)
+
+    function renderImage(dogImage) {
+      const container = document.getElementById('dog-image-container')
+      const img = document.createElement('img')
+      img.src = dogImage
+      container.append(img)
+    }
+
+    function renderBreeds(breeds) {
+      breeds.forEach(breed => {
+        let li = document.createElement('li')
+        li.textContent = breed
+        ul.append(li)
+        li.addEventListener('click', changeColor)
+      })
+    }
+    function changeColor(e) {
+      e.target.style.color = "red"
+    }
+
+    function handleChange(e) {
+      const letter = e.target.value
+      filteredBreeds = breeds.filter(breed => breed.startsWith(letter))
+      ul.innerHTML = ''
+      renderBreeds(filteredBreeds)
+    }
+})
